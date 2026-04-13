f15d8c9f508e38241b082945f42944c290b980b8
Boruto-code <hamburger_nb_666@outlook.com>
2026-04-13 19:17:26 +0800

    Update Tooltip Update Mods

--- kubejs/client_scripts/tooltip.js
+++ kubejs/client_scripts/tooltip.js
@@ -1,3 +1,7 @@
 ItemEvents.tooltip(event => {
     event.add("kubejs:dimensional_teleporter", "右键传送超平坦维度")
+    event.addAdvanced("kubejs:supracausal_mainframe", (item, advanced, text) => {
+        text.add(1, Text.of("§7万物归一者，原初的神迹"))
+        text.add(2, Text.red("MAX级电路"))
+    })
 })
